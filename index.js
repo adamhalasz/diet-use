9cb1a4fb48e993325222a6f73d5d59388d0588a2
Adam Halasz <adamfsh@gmail.com>
2014-08-02 02:53:28 +0200

    v0.0.3

--- index.js
+++ index.js
@@ -6,37 +6,35 @@
 		- require module
 		- require directory
 	*/
-	
+	var fs = require('fs');
 	module.exports = function(base_path){
-		//console.log('base_path = ', base_path);
 		return function(Path){
-			
-			var path = base_path + '/' + Path;
+			var $path = base_path + '/' +Path;
 			try {
-				var stats = fs.lstatSync(path);
+				var stats = fs.lstatSync($path);
 				
 				// REQUIRE DIRECTORY
 				if(stats.isDirectory()){
-					var files = fs.readdirSync(path);
+					var files = fs.readdirSync($path);
 					
 					for(var i = 0; i < files.length; i++){ 
-						var file = path+'/'+files[i];
+						var file = $path+'/'+files[i];
 						var file_stats = fs.lstatSync(file);
-						if(file_stats.isFile()){ require(file); } 
+						if(file_stats.isFile() && path.extname($path+'/'+files[i]) == '.js'){ require(file); } 
 					}
 				}
 				
 				// REQUIRE FILE
 				if(stats.isFile()){
-					require(path);
+					require($path);
 				} 
 				
 			} catch (error) {
 				if(error.code == 'ENOENT'){
 					// REQUIRE MODULE
-					return require(path);
+					return require($path);
 				} else {
-					console.log(error);
+					throw new Error(error);
 					return null;
 				}
 			}
